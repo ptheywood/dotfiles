37d44de723894769827b0fef108486c80ba85678
Peter Heywood <peethwd@gmail.com>
2022-04-27 14:35:21 +0100

    Add nvm loading if  installed in home directory

--- .config/bashrc.d/03-environment.bash
+++ .config/bashrc.d/03-environment.bash
@@ -1,1 +1,8 @@
 export PATH="$HOME/bin:$HOME/bin/bash:$HOME/.local/bin/:$PATH"
+
+
+if [ -d "$HOME/.nvm" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
